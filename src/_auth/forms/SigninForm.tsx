eedaa9604f6704428ba77ea4ee36f50c3e52a986
Michael Noon <m.noon83@yahoo.com>
2024-04-28 17:38:12 -0700

    changed name on signup

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -56,11 +56,11 @@ const SigninForm = () => {
       <div className="sm:w-420 flex-center flex-col">
         <img src="/assets/images/logo.svg" alt="logo" />
 
-        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
-          Log in to your account
-        </h2>
+        <h6 className="h3-bold md:h2-bold pt-5 sm:pt-12">
+          Welcum back
+        </h6>
         <p className="text-light-3 small-medium md:base-regular mt-2">
-          Welcome back! Please enter your details.
+          Log in here
         </p>
         <form
           onSubmit={form.handleSubmit(handleSignin)}
@@ -104,11 +104,11 @@ const SigninForm = () => {
           </Button>
 
           <p className="text-small-regular text-light-2 text-center mt-2">
-            Don&apos;t have an account?
+            Need an account?
             <Link
               to="/sign-up"
               className="text-primary-500 text-small-semibold ml-1">
-              Sign up
+              Sign up here
             </Link>
           </p>
         </form>
